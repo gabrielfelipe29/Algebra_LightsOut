7525cb9eed09966812e44c54e1c39a29b7828484
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-11-01 19:41:06 -0300

    Agregando verificaciones

--- tarea3.py
+++ tarea3.py
@@ -5,6 +5,18 @@ import random
 
 
 def posicion_valida(n, m, dim):
+    # n: es la fila
+    # m: es la columna
+    # dim: dimension de la matriz
+
+    if n is None or m is None or dim is None:
+        print("Faltan parametros")
+        return
+
+    if dim < 0:
+        print("Dimensión tiene que ser mayor que 0.")
+        return
+
     if (n >= 0 and n < dim) and (m >= 0 and m < dim):
         return True
     return False
@@ -14,7 +26,15 @@ def posicion_valida(n, m, dim):
 def posiciones_afectadas(n, m, dim):
     # n: es la fila
     # m: es la columna
-    # dim: dimension de la matriz (se supone que es cuadrada)
+    # dim: dimension de la matriz
+    if n is None or m is None or dim is None:
+        print("Faltan parametros")
+        return
+
+    if n < 0 or m < 0 or dim < 0:
+        print("Los parametros tienen que ser mayores que 0. 2")
+        return
+
     posiciones = [[n, m], [n, m - 1], [n - 1, m], [n + 1, m], [n, m + 1]]
 
     validas = []
@@ -31,6 +51,14 @@ def generar_posiciones_afectada(n, m, dim, posiciones):
     # m: es la columna
     # dim: dimension de la matriz (se supone que es cuadrada)
     # posiciones: son todas las posiciones posibles de la matriz
+    if n is None or m is None or dim is None or posiciones is None:
+        print("Faltan parametros")
+        return
+
+    if n < 0 or m < 0 or dim < 0:
+        print("Los parametros tienen que ser mayores que 0.")
+        return
+
     fila = []
     afectadas = posiciones_afectadas(n, m, dim)
     for pos in posiciones:
@@ -67,16 +95,29 @@ def obtener_posicion(solucion):
 
 # Imprime la matriz en lineas distintas
 def imprimir_matriz(matriz):
-    for i in range(0, len(matriz), n):
-        print(matriz[i:i + n])
+    if matriz is None or len(matriz) == 0:
+        return
+
+    matriz = sp.Matrix(matriz)
+    for row in matriz.tolist():  # Convert matrix to a list of lists
+        print(row)
 
 
 # Resuelve el sistema de ecuaciones y devuelve la matriz
 def generar_solucion(matriz):
     # matriz: es la matriz inicial, con 0 o 1
+
+    if matriz is None:
+        print("La matriz es nula")
+        return
+    matriz = sp.Matrix(matriz)
+    if matriz.shape[0] != matriz.shape[1]:
+        print("La matriz no es cuadrada")
+        return
+
     posiciones = []  # todas las posiciones de la matriz
     vector_b = []
-    # se obtiene la dimensión. Se asume que la matriz es cuadrada.
+    # se obtiene la dimensión.
     dim = matriz.shape[0]
     matriz_ecuacion = []
     # genera todas las posiciones de la matriz
@@ -97,9 +138,17 @@ def eliminacion_gauss(matriz, vector_b):
     # matriz: la matriz de movimientos
     # vector_b: es el vector con el valor de cada posicion de la matriz original
 
+    if matriz is None or vector_b is None:
+        print("Faltan parametros")
+        return
+
     # Transforma la matriz y vector_b en matrices
     b = sp.Matrix(vector_b)
     A = sp.Matrix(matriz)
+
+    if b.shape[1] > 1 or b.shape[0] != A.shape[0]:
+        print("El vector constante tiene tamano incorrecto")
+        return
 
     # Cantidad de filas de la matriz
     n = A.shape[0]
@@ -136,37 +185,48 @@ def eliminacion_gauss(matriz, vector_b):
     return solucion
 
 
-# matriz = generar_matriz_invertible(4)
+def main():
+    # matriz = generar_matriz_invertible(4)
+    
+    matriz = [[0, 1, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
+    
+    """
+    matriz = [[0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0]]
+    """
+    """
+    matriz = [[0, 1, 1, 1, 1],
+            [1, 0, 1, 1, 0],
+            [0, 1, 1, 0, 0],
+            [1, 1, 1, 1, 1],
+            [1, 0, 1, 0, 1]]
 
-matriz = sp.Matrix([[0, 0, 0],
-                   [1, 1, 1],
-                   [0, 1, 0]])
+    matriz = [
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1],
+        [1, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 0, 1, 0, 1],
+        [0, 0, 0, 1, 0, 1, 0]
+    ]
+    """
 
-"""
-matriz = sp.Matrix([[0, 1, 1, 1, 1],
-                   [1, 0, 1, 1, 0],
-                   [0, 1, 1, 0, 0],
-                   [1, 1, 1, 1, 1],
-                   [1, 0, 1, 0, 1]])
-"""
+    n = sp.Matrix(matriz).shape[0]
+    print("Matriz original: ")
+    imprimir_matriz(matriz)
+    solucion = generar_solucion(matriz)
+    matriz = sp.Matrix(matriz)
 
-"""
-matriz = sp.Matrix([
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 1, 0, 1, 1, 0, 0],
-    [0, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 1, 0, 0, 0],
-    [1, 1, 0, 0, 1, 0, 1],
-    [0, 0, 0, 1, 0, 1, 0]
-])
-"""
+    if matriz.shape[0] == matriz.shape[1] and solucion is not None:
+        print("Solucion como vector: ", solucion)
+        print("Solucion como matriz: ")
+        imprimir_matriz(sp.Matrix(solucion).reshape(n, n))
+        print("Posiciones a presionar: ", obtener_posicion(solucion))
 
-n = matriz.shape[0]
-print("Matriz original: ")
-imprimir_matriz(matriz)
-solucion = generar_solucion(matriz)
-print("Solucion como vector: ", solucion)
-print("Solucion como matriz: ")
-imprimir_matriz(sp.Matrix(solucion).reshape(n, n))
-print("Posiciones a presionar: ", obtener_posicion(solucion))
+
+if __name__ == "__main__":
+    main()
